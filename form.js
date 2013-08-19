58b76b5673efccac178fb605c2bd6ce40aaabe4e
Alex <lumen@rodionova.net>
2013-08-19 02:04:22 -0700

    Update form.js

--- form.js
+++ form.js
@@ -1,10 +1,7 @@
 /* Основной скрипт анкеты */
 $(function() {
-
 	var fieldsetCount = $('#formElem').children().length;
-	
 	var current 	= 1;
-    
 	var stepsWidth	= 0;
     var widths 		= new Array();
 	$('#steps .step').each(function(i){
@@ -13,19 +10,14 @@ $(function() {
         stepsWidth	 	+= $step.width();
     });
 	$('#steps').width(stepsWidth);
-	
 	$('#formElem').children(':first').find(':input:first').focus();	
-	
 	$('#navigation').show();
-	
     $('#navigation a').bind('click',function(e){
 		var $this	= $(this);
 		var prev	= current;
 		$this.closest('ul').find('li').removeClass('selected');
         $this.parent().addClass('selected');
-
 		current = $this.parent().index() + 1;
-
         $('#steps').stop().animate({
             marginLeft: '-' + widths[current-1] + 'px'
         },500,function(){
@@ -49,18 +41,15 @@ $(function() {
 		});
 	});
 	
-	$("input[type='number']").keydown(function(event) {
-        if ( event.keyCode == 46 || event.keyCode == 8 || event.keyCode == 9 || event.keyCode == 27 || 
-            (event.keyCode == 65 && event.ctrlKey === true) || 
-            (event.keyCode >= 35 && event.keyCode <= 39)) {
-                 return;
-        }
-        else {
-            if ((event.keyCode < 48 || event.keyCode > 57) && (event.keyCode < 96 || event.keyCode > 105 )) {
-                event.preventDefault(); 
-            }   
-        }
-    });
+	function validateEmail(email) {
+		var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+		if( !emailReg.test( email ) ) {
+			return false;
+		}
+		else {
+			return true;
+		}
+	}
 	
 	function validateSteps(){
 		var FormErrors = false;
@@ -79,23 +68,33 @@ $(function() {
 		var hasError = false;
 		$('#formElem').children(':nth-child('+ parseInt(step) +')').find(':input[data-type="validate"]').each(function(){
 			var $this 		= $(this);
-			var valueLength = jQuery.trim($this.val()).length;			
+			var valueLength = jQuery.trim($this.val()).length;
+			
+			if ($this.attr("data") == "mail_check") {
+				if(!validateEmail(String($this.val()))){
+					hasError = true;
+					$this.css('background-color','#FFEDEF');
+					}
+				else
+					$this.css('background-color','#FFFFFF');
+			}
+			
 			if(valueLength == ''){
 				hasError = true;
 				$this.css('background-color','#FFEDEF');
 			}
 			else
-				$this.css('background-color','#FFFFFF');
+				$this.css('background-color','#FFFFFF');	
 		});
 		var $link = $('#navigation li:nth-child(' + parseInt(step) + ') a');
 		$link.parent().find('.error,.checked').remove();
 		
-		var valclass = 'checked_mark';
+		var valclass = 'checked';
 		if(hasError){
 			error = -1;
-			valclass = 'error_mark';
+			valclass = 'error';
 		}
-		$link.addClass(valclass);
+		$('<span class="'+valclass+'"></span>').insertAfter($link);
 		
 		return error;
 	}
@@ -109,7 +108,9 @@ $(function() {
 		}
 	});
 	
+
 	$('#registerButton').bind('click',function(){
+		validateSteps();
 		$this = $('input[name="yes"]');
 		if (!$this.is(':checked')){
 			$(".error_check").fadeIn();
@@ -119,6 +120,7 @@ $(function() {
 		else
 		{
 			$(".error_check").fadeOut();
+			FormErrors = false;
 		}
 		$this = $('select[name="ad"]');
 		if ($this.val() == "no"){
@@ -129,6 +131,7 @@ $(function() {
 		else
 		{
 			$(".error_radio").fadeOut();
+			FormErrors = false;
 		}
 		if($('#formElem').data('errors')){
 			alert('Имеются некорректно заполненные поля');
